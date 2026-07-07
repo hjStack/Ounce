cb7522e5663613c6835103fb95b1696ac388120d
hjStack <minnie1498@naver.com>
2026-07-07 21:48:55 +0900

    fix: 구글 로그인 후 로컬로 되돌아 오는 문제 해결

--- src/main/java/ounce/market/demo/product/entity/Category.java
+++ src/main/java/ounce/market/demo/product/entity/Category.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Category {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
