f5ec090004bd674ce2b56e39604753275ff87f97
hjStack <minnie1498@naver.com>
2026-08-19 14:48:00 +0900

    feat:멤버에서 grade 칼럼 삭제

--- src/main/java/ounce/market/demo/member/dto/response/MemberResponse.java
+++ src/main/java/ounce/market/demo/member/dto/response/MemberResponse.java
@@ -12,17 +12,14 @@ public class MemberResponse {
     private String email;
     private int point; // 💡 화면에 보여줘야 하니까 여기엔 꼭 포함!
     private String name;
-    private String grade;
-
     // Entity -> DTO 변환 편의 메서드
-    public static MemberResponse from(Member member,String grade) {
+    public static MemberResponse from(Member member) {
 
         return new MemberResponse(
                 member.getMemberId(),
                 member.getEmail(),
                 member.getPoint(),
-                member.getName(),
-                member.getGrade()
+                member.getName()
         );
     }
 }
